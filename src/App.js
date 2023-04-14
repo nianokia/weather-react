04dcbbd38e03bef85f1fc33b0a2169695b851190
nianokia <111546789+nianokia@users.noreply.github.com>
2023-04-14 15:20:34 -0400

    Deleted Search.js & integrated it to Weather.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Weather from "./Weather";
-import Search from "./Search";
 import "bootstrap/dist/css/bootstrap.css";
 
 import "./App.css";
@@ -8,7 +7,6 @@ export default function App() {
   return (
     <div className="App">
       <div className="appContainer">
-        <Search />
         <Weather defaultCity="Atlanta" />
       </div>
       <footer>
--- src/Weather.js
+++ src/Weather.js
@@ -8,6 +8,7 @@ import "./Weather.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+
   function handleResponse(response) {
     setWeatherData({
       ready: true,
@@ -20,9 +21,33 @@ export default function Weather(props) {
     });
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   if (weatherData.ready) {
     return (
       <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                className="search-box form-control"
+                type="search"
+                placeholder="Enter a city..."
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                className="btn btn-warning btn-search form-control w-100"
+                type="submit"
+                value="Search"
+              />
+            </div>
+          </div>
+        </form>
+
         <div className="heading">
           <h1>{weatherData.name}</h1>
           <p>
