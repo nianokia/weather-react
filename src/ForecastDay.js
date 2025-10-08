142c57978188fbceee6846a40ecaad0062e9f416
Nia Wright <nw.niawright@gmail.com>
2025-10-08 13:58:40 -0400

    Update prop definitions to conform to new weather API

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -3,7 +3,7 @@ import WeatherIcon from "./WeatherIcon";
 
 export default function ForecastDay(props) {
   function day() {
-    let date = new Date(props.data.dt * 1000);
+    let date = new Date(props.data.time * 1000);
 
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     let day = days[date.getDay()];
@@ -11,12 +11,12 @@ export default function ForecastDay(props) {
   }
 
   function maxTemp() {
-    let max = Math.round(props.data.temp.max);
+    let max = Math.round(props.data.temperature.maximum);
     return `${max}°`;
   }
 
   function minTemp() {
-    let min = Math.round(props.data.temp.min);
+    let min = Math.round(props.data.temperature.minimum);
     return `${min}°`;
   }
 
@@ -24,10 +24,10 @@ export default function ForecastDay(props) {
     <div className="ForecastDay">
       <div className="ForecastDayName">{day()}</div>
       <div className="d-none d-md-block">
-        <WeatherIcon code={props.data.weather[0].icon} size={60} />
+        <WeatherIcon code={props.data.condition.icon} size={60} />
       </div>
       <div className="d-lg-none d-md-none d-sm-block">
-        <WeatherIcon code={props.data.weather[0].icon} size={45} />
+        <WeatherIcon code={props.data.condition.icon} size={45} />
       </div>
       <div className="forecastTemperatures">
         <span className="forecastTemperatureMax">{maxTemp()}</span>
