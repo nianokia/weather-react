b044ecd729eca575a3d79de85b128ebea685bd95
Nia Wright <nw.niawright@gmail.com>
2025-10-08 13:55:59 -0400

    Change weather api and import apiKey as an environment variable

--- src/Weather.js
+++ src/Weather.js
@@ -11,24 +11,25 @@ export default function Weather(props) {
   const [city, setCity] = useState(props.defaultCity);
 
   function search() {
-    const apiKey = "7fbc4204c23b04814101fc2bf335b695";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    const apiKey = process.env.REACT_APP_WEATHER_APIKEY;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
     axios.get(apiUrl).then(handleResponse);
   }
 
   function handleResponse(response) {
     setWeatherData({
       ready: true,
-      name: response.data.name,
-      temperature: response.data.main.temp,
-      description: response.data.weather[0].description,
-      humidity: response.data.main.humidity,
+      name: response.data.city,
+      temperature: response.data.temperature.current,
+      description: response.data.condition.description,
+      humidity: response.data.temperature.humidity,
       wind: response.data.wind.speed,
-      icon: response.data.weather[0].icon,
-      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-      timezone: response.data.timezone,
-      coordinates: response.data.coord,
+      icon: response.data.condition.icon,
+      iconUrl: response.data.condition.icon_url,
+      timezone: response.data.time,
+      coordinates: response.data.coordinates,
     });
+    console.log(`weatherData: ${JSON.stringify(weatherData.coordinates)}`);
   }
 
   function handleSubmit(event) {
