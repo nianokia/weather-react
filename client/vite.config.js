7eacb65f8ad01ea2cb88b4a6f054da9c5c57f919
Nia Wright <nw.niawright@gmail.com>
2025-10-09 15:27:06 -0400

    Add build configuration to vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
+    build: {
+      outDir: 'dist',
+    },
     plugins: [react()],
     server: {
       proxy: {
